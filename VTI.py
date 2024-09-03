64e3a09f65fed3225f207c4bd581eb38845a825d
kangsiwoo <2023003@bssm.hs.kr>
2024-09-03 19:34:35 +0900

    feat: Optimization

--- VTI.py
+++ VTI.py
@@ -2,6 +2,7 @@ import cv2
 import os
 
 def extract_frames_from_videos(video_folder, output_folder, frame_rate):
+    # 비디오 파일 리스트 생성
     video_files = [f for f in os.listdir(video_folder) if f.endswith(('.mp4', '.avi', '.mov', '.mkv', '.MOV'))]
 
     for video_file in video_files:
@@ -12,17 +13,25 @@ def extract_frames_from_videos(video_folder, output_folder, frame_rate):
         os.makedirs(output_video_folder, exist_ok=True)
 
         video_cap = cv2.VideoCapture(video_path)
-        count = 0
-        success, image = video_cap.read()
 
-        while success:
-            if count % frame_rate == 0:
-                frame_filename = os.path.join(output_video_folder, f"frame_{count}.jpg")
-                cv2.imwrite(frame_filename, image)
+        total_frames = int(video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        count = 0
+
+        while count < total_frames:
+            video_cap.set(cv2.CAP_PROP_POS_FRAMES, count)
             success, image = video_cap.read()
-            count += 1
+
+            if not success:
+                break
+
+            frame_filename = os.path.join(output_video_folder, f"frame_{count}.jpg")
+            cv2.imwrite(frame_filename, image)
+            print(f"Saved frame {count} from {video_file}")
+
+            count += frame_rate
 
         video_cap.release()
         print(f"Extracted frames from {video_file} are saved in {output_video_folder}")
 
-extract_frames_from_videos('./VideoInput', './ImagesOutput', 2)
+extract_frames_from_videos('./VideoInput', './ImagesOutput', 60)
